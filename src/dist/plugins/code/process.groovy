645d9075d02b88d5002b820c750fcc83ee6aa65b
enr <enrico.comiti@gmail.com>
2010-05-18 11:30:33 +0200

    fix for encoding

--- src/dist/plugins/code/process.groovy
+++ src/dist/plugins/code/process.groovy
@@ -3,6 +3,7 @@ import org.slf4j.LoggerFactory;
 import xite.Strings;
 import xite.Files;
 
+import com.petebevin.markdown.Entities
 
 import xite.HtmlDirectoryLister
 
@@ -107,7 +108,7 @@ for (codeDir in codeSourceDirectories) {
       }
       //def ics = wrapLines(src.text.toString())
       def ics = src.text.toString()
-      def codeString = Strings.htmlEscape(ics)
+      def codeString = Strings.htmlEscape(ics, Entities.HTML_401)
       def content = String.format(configuration.code.template, lang, codeString)
       def finalContent = Strings.normalizeEol("${header}${content}${footer}")
       //logger.warn(finalContent)
