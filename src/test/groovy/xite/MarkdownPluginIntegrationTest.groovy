a5b963912491d893000de18302fa06d0437eae3c
enr <enrico.comiti@gmail.com>
2010-05-18 12:11:48 +0200

    added test for special chars escaping

--- src/test/groovy/xite/MarkdownPluginIntegrationTest.groovy
+++ src/test/groovy/xite/MarkdownPluginIntegrationTest.groovy
@@ -48,7 +48,30 @@ public class MarkdownPluginIntegrationTest extends BasePluginIntegrationTest
     public void testExcludedExtension()
     {   
     	File exc = new File(targetDir.getAbsolutePath()+'/excluded.html');
-        assertFalse(exc.exists(), "${exc.getAbsolutePath()} not found");
+        assertFalse(exc.exists());
     }
+        
+    /*
+     * 
+     */
+    @Test
+    public void testEscaping()
+    {   
+    	File escaped = new File(targetDir.getAbsolutePath()+'/special-chars.html');
+        assertTrue(escaped.exists(), "${escaped.getAbsolutePath()} not found");
+    	def actualEscaped = Strings.normalizeEol(escaped.text)
+        def expectedEscaped = '''<html>
+<head></head>
+<body>
 
+<h1>Xite</h1>
+
+<p>Xite &#236;s &#224; stati&#231; sit&#233;s generat&#242;r.
+It &#249;ses &#60;Groovy&#62; &#38; &#60;Gradle&#62;.</p>
+</body>
+</html>
+'''
+        expectedEscaped = Strings.normalizeEol(expectedEscaped)
+        assertEquals actualEscaped, expectedEscaped
+    }
 }
